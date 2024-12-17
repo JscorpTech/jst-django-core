14bd0fb2023255e3980dcb09cb589dd41a30b4f5
A'zamov Samandar <JscorpTech@gmail.com>
2024-12-17 13:01:38 +0500

    o'zgarishlar: Flake8 konfiguratsiyasi qo'shildi; versiya yangilandi; SMS xizmati uchun muhit o'zgaruvchilari yangilandi.

--- src/django_core/tasks/sms.py
+++ src/django_core/tasks/sms.py
@@ -5,7 +5,7 @@ Base celery tasks
 from importlib import import_module
 
 from celery import shared_task
-from config.env import env
+import os
 from django.utils.translation import gettext as _
 import logging
 
@@ -13,9 +13,15 @@ import logging
 @shared_task
 def SendConfirm(phone, code):
     try:
-        service = getattr(import_module(env.str("OTP_MODULE")), env.str("OTP_SERVICE"))()
-        service.send_sms(phone, _("Sizning Tasdiqlash ko'dingiz: %(code)s") % {"code": code})
+        service = getattr(
+            import_module(os.getenv("OTP_MODULE")), os.getenv("OTP_SERVICE")
+        )()
+        service.send_sms(
+            phone, _("Sizning Tasdiqlash ko'dingiz: %(code)s") % {"code": code}
+        )
         logging.info("Sms send: %s-%s" % (phone, code))
     except Exception as e:
-        logging.error("Error: {phone}-{code}\n\n{error}".format(phone=phone, code=code, error=e))  # noqa
+        logging.error(
+            "Error: {phone}-{code}\n\n{error}".format(phone=phone, code=code, error=e)
+        )  # noqa
         raise Exception("Sms yuborishda xatolik yuzaga keldi")
--- src/django_core/templatetags/vite.py
+++ src/django_core/templatetags/vite.py
@@ -1,7 +1,7 @@
 import json
 
-from config.env import env
 from django.conf import settings
+import os
 from django.templatetags import static
 from django.utils import safestring
 
@@ -13,15 +13,17 @@ register = template.Library()
 def getScript(url: object) -> str:
     ext: str = str(url).split(".")[-1]
 
-    if env("VITE_LIVE"):
-        url = f"http://{env('VITE_HOST')}:{env('VITE_PORT')}/{url}"
+    if os.getenv("VITE_LIVE", False):
+        url = f"http://{os.getenv('VITE_HOST', "0.0.0.0")}:{os.getenv('VITE_PORT', 5000)}/{url}"
     else:
         url = static.static(f"vite/{url}")
 
     if ext == "css":
         script = f"<link rel='stylesheet' type='text/css' href='{url}'>"
     else:
-        script = ("<script type='module' type='text/javascript' src='{" "}'></script>").format(url)
+        script = (
+            "<script type='module' type='text/javascript' src='{" "}'></script>"
+        ).format(url)
     return script
 
 
@@ -35,12 +37,12 @@ def vite_load(*args):
             f"Vite manifest file not found or invalid. Maybe your"
             f" {settings.VITE_APP_DIR}/manifest.json file is empty?"
         )
-    if not env("VITE_LIVE"):
+    if not os.getenv("VITE_LIVE", False):
         imports_files = "".join([getScript(file["file"]) for file in manifest.values()])
 
     else:
         imports_files = "".join([getScript(file) for file in args])
-        imports_files += f""" <script type="module" src="http://{env('VITE_HOST')}:{env('VITE_PORT')}/@vite/client">
+        imports_files += f""" <script type="module" src="http://{os.getenv('VITE_HOST', "0.0.0.0")}:{os.getenv('VITE_PORT', 5000)}/@vite/client">
         </script> <script type="module" src="{static.static("js/vite-refresh.js")}"></script>"""
 
     return safestring.mark_safe(imports_files)
