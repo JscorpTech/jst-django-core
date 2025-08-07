95440bcb74ddcb13fe7e6a255b9665683cdb6f68
A'zamov Samandar <admin@jscorp.uz>
2025-08-07 12:09:45 +0500

    fix typo

--- src/django_core/mixins/base.py
+++ src/django_core/mixins/base.py
@@ -10,17 +10,14 @@ class BaseViewSetMixin(object):
         if response.status_code == 204:
             return super().finalize_response(request, response, *args,
                                              **kwargs)
-        message = self._extract_first_error(response.data)
         if response.status_code >= 400:
             response.data = {
                 "status": False,
-                "message": message,
-                "data": response.data
+                "data": response.data,
             }
         else:
             response.data = {
                 "status": True,
-                "message": message,
                 "data": response.data,
             }
 
@@ -35,11 +32,3 @@ class BaseViewSetMixin(object):
             permission() for permission in self.action_permission_classes.get(
                 self.action, self.permission_classes)
         ]
-
-    def _extract_first_error(self, data):
-        for key, value in data.items():
-            if isinstance(value, list) and value:
-                return str(value[0])
-            elif isinstance(value, str):
-                return value
-        return "Invalid data"
